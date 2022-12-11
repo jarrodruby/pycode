786d40db4f8341266247c7c8f4c798ae0eb7906b
jarrodruby <116897214+jarrodruby@users.noreply.github.com>
2022-12-11 18:21:05 +0000

    Update second_highest_number_sub_functions.py

--- second_highest_number_sub_functions.py
+++ second_highest_number_sub_functions.py
@@ -2,41 +2,37 @@ import unittest
 
 def remove_non_integers(nums):
     # In-built `filter` function
-    # https://www.programiz.com/python-programming/methods/built-in/filter
-    # N.B. It returns a Tuple
-
-    # Keyword lambda (annoymous function)
-    # https://www.w3schools.com/python/python_lambda.asp
-    filtered_as_tuple = filter(lambda num: isinstance(num, int), nums)
+    # https://www.w3schools.com/python/ref_func_filter.asp
+    filtered_as_tuple = filter(
+        # Keyword lambda (annoymous function)
+        # https://www.w3schools.com/python/python_lambda.asp
+        lambda num: isinstance(num, int), nums
+    )
 
     # In-built `list` function
-    # https: // www.programiz.com / python - programming / methods / built - in / list
+    # https://www.w3schools.com/python/ref_func_list.asp
     filtered_as_list = list(filtered_as_tuple)
-
-    # Side notes (not essiential)
-    # Difference between a tuple and a list
-    # https: // www.programiz.com / python - programming / list - vs - tuples
 
     return filtered_as_list
 
 def sort_list_of_integers(unsorted_nums):
     # In-built `sorted` function
-    # https: // www.programiz.com / python - programming / methods / built - in / sorted
+    # https://www.w3schools.com/python/ref_func_sorted.asp
     sorted_nums = sorted(unsorted_nums)
 
     return sorted_nums
 
 def second_last_element_of_list(nums):
     # In-built `len` function
-    # https://www.programiz.com/python-programming/methods/built-in/len
+    # https://www.w3schools.com/python/ref_func_len.asp
     list_size = len(nums)
 
     # Arithmetic operator
-    # https://www.geeksforgeeks.org/python-arithmetic-operators/
+    # https://www.w3schools.com/python/python_operators.asp
     index_of_second_last_element = list_size - 2
 
     # Access element from list
-    # https://www.w3schools.com/python/gloss_python_access_list_items.asp
+    # https://www.w3schools.com/python/gloss_python_array_access.asp
     result = nums[index_of_second_last_element]
 
     return result
@@ -117,4 +113,3 @@ class SimpleTestCase(unittest.TestCase):
 
 if __name__ == "__main__":
     unittest.main()
-    
